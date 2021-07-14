4ea93845261b9e492afa5cc2c2bc2385e79f20c2
Martin Jalid <martin.jalid@mercadolibre.com>
2021-07-14 15:03:24 -0300

    pedidos endpoint buscar

---
+++ Models/ArticulosPedidoDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace claseServicio.Models
+{
+    public partial class PedidoArticuloDTO
+    {
+        public int IdArticulo { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ Models/ModificadoPorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace claseServicio.Models
+{
+    public partial class ModificadoPorDTO
+    {
+        public string Email { get; set; }
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
